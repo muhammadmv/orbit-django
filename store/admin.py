a868f0e0267018192360b59b6149e09703aad6b4
muhammadmv <muhammad144005@gmail.com>
2021-10-19 00:04:35 +0500

    review and rating

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from .models import Product, Variation
+from .models import Product, ReviewRating, Variation
 
 # Register your models here.
 
@@ -20,3 +20,4 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)
