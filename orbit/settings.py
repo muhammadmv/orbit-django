1bb6062dc364ef6f25ee0bed9281ea92d0410ca4
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2021-11-27 06:58:56 +0500

    allowed host

--- orbit/settings.py
+++ orbit/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)  # True
 
-ALLOWED_HOSTS = ['django-biz.eba-ztg2i2mu.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['orbitcourse-env.eba-suymsgn9.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
