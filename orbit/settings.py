24f086daa1438b04f2275294fd80e64f12c56201
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2021-11-27 23:49:13 +0500

    postgres configuration

--- orbit/settings.py
+++ orbit/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)  # True
 
 ALLOWED_HOSTS = ['orbitcourse-env.eba-suymsgn9.us-west-2.elasticbeanstalk.com']
-
+# orbitcourse-env.eba-suymsgn9.us-west-2.elasticbeanstalk.com
 
 # Application definition
 
